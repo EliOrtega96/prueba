f3b83c134c78d5ebbb6e453a500d0885496d7f86
Eli <elixzhita_ortega@hotmail.com>
2018-12-11 16:15:08 -0600

    creacion del login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hola', function () {
+    return "holamundo";
 });
+
+Route::get('/', function () {
+    return view("welcome");
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
